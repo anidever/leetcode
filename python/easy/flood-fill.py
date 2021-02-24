958ea3f93cecf9bcad2fd1339e6cf07b077a713e
anidever <anidever@outlook.com>
2021-02-23 23:52:02 -0500

    flood-fill

---
+++ python/easy/flood-fill.py
@@ -0,0 +1,41 @@
+from typing import List
+
+
+class Solution:
+    def floodFill(
+        self, image: List[List[int]], sr: int, sc: int, newColor: int
+    ) -> List[List[int]]:
+        rows, cols = len(image), len(image[0])
+        oldColor = image[sr][sc]
+        # the use of seen/visited container is optional here
+
+        def isValid(row, col):
+            return (rows > row >= 0 and cols > col >= 0) and image[row][col] == oldColor
+
+        # DFS solution
+
+        def dfs(row, col):
+            if isValid(row, col):
+                image[row][col] = newColor
+                [dfs(row + x, col + y) for x, y in [(1, 0), (-1, 0), (0, 1), (0, -1)]]
+
+        if oldColor != newColor:
+            dfs(sr, sc)
+        return image
+
+        # BFS solution
+        if oldColor != newColor:
+            queue = [(sr, sc)]
+            while queue:
+                row, col = queue.pop(0)
+                image[row][col] = newColor
+                for row, col in [
+                    (row - 1, col),
+                    (row + 1, col),
+                    (row, col - 1),
+                    (row, col + 1),
+                ]:
+                    if isValid(row, col):
+                        queue.append((row, col))
+
+            return image
