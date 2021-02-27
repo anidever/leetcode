db01a4948645d1325ebaa2c53248fb11459b89c1
anidever <anidever@outlook.com>
2021-02-27 11:10:35 -0500

    max-area-of-island

---
+++ python/medium/max-area-of-island.py
@@ -0,0 +1,47 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        max_area = 0
+        rows, cols = len(grid), len(grid[0])
+
+        def isValid(row, col):
+            return cols > col >= 0 and rows > row >= 0 and grid[row][col]
+
+        def dfs(row, col):
+            area = 1
+            grid[row][col] = 0
+            for row, col in (
+                (row - 1, col),
+                (row + 1, col),
+                (row, col - 1),
+                (row, col + 1),
+            ):
+                if isValid(row, col):
+                    grid[row][col] = 0
+                    area += dfs(row, col)
+
+            return area
+
+        def bfs(row, col):
+            area = 1
+            grid[row][col] = 0
+            queue = [(row, col)]
+            while queue:
+                row, col = queue.pop(0)
+                for row, col in (
+                    (row - 1, col),
+                    (row + 1, col),
+                    (row, col - 1),
+                    (row, col + 1),
+                ):
+                    if isValid(row, col):
+                        area += 1
+                        grid[row][col] = 0
+                        queue.append((row, col))
+            return area
+
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col]:
+                    max_area = max(max_area, dfs(row, col))
+
+        return max_area
