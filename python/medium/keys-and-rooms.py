9364018976c156c6724a0a63b6bcc960cfefb9f9
anidever <anidever@outlook.com>
2021-02-28 20:25:02 -0500

    keys-and-rooms

---
+++ python/medium/keys-and-rooms.py
@@ -0,0 +1,16 @@
+# question can be found at https://leetcode.com/problems/keys-and-rooms/
+from typing import List
+
+
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        keys = [key for key in rooms[0]]
+        visited = {0}
+        while keys:
+            key = keys.pop(0)
+            visited.add(key)
+            for room in rooms[key]:
+                if room not in visited:
+                    keys.append(room)
+
+        return len(rooms) == len(visited)
