a3482404714dcb36f1c62ed7bb0ceaba2ad129e3
Ani Dever <anidever@outlook.com>
2022-10-07 15:04:59 -0400

    max depth of binary tree

--- python/easy/maximum-depth-of-binary-tree.py
+++ python/easy/maximum-depth-of-binary-tree.py
@@ -2,11 +2,9 @@
 from typing import Optional
 from abstract_data_types.binary_tree import TreeNode
 
-
+# Recursive Solution
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        # for this purpose DFS & recursion will likely to perform better,
-        # but could be solved with BFS & stack as well
         if not root:
             return 0
 
@@ -14,3 +12,25 @@ class Solution:
         right_depth = self.maxDepth(root.right)
 
         return max(left_depth, right_depth) + 1
+
+# Iterative Solution
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        queue = [root]
+        result = 0
+
+        while queue:
+            size = len(queue)
+            for _ in range(size):
+                node = queue.pop(0)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            result += 1
+
+        return result
